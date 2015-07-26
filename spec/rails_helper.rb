89231d7b3d76cbb6a9b3e7689eb785a0581f4de2
Kelly Raila <kelly.raila@gmail.com>
2015-07-26 19:29:46 -0400

    remove requirement of capybara webkit

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,8 +6,6 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 require 'spec_helper'
 require 'rspec/rails'
 require 'capybara/rspec'
-require 'capybara/webkit/matchers'
-Capybara.javascript_driver = :webkit
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
