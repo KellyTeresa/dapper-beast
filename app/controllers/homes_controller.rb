90de6a2d6f33bf3ade58ac6dfb8173d7e9347b2b
Kelly Raila <kelly.raila@gmail.com>
2015-10-15 23:13:52 -0400

    style most recent post div to resemble tumblr, give other font my 'signature' Josefin Sans look

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,7 +1,7 @@
 class HomesController < ApplicationController
   def index
     @projects = Link.where(project: true)
-    @other = Link.where(project: false)
+    @other = Link.where(project: false).order('name ASC')
 
     client = Tumblr::Client.new :consumer_key => ENV["TUMBLR_AUTH"]
     most_recent_post_data = client.posts 'kellyraila.tumblr.com', :limit => 1
--- db/seeds.rb
+++ db/seeds.rb
@@ -4,19 +4,26 @@ link_list = [
   [
     "Gender Friendly Adventures",
     "https://genderfriendlyadventures.herokuapp.com/",
-    true
+    true,
+    "https://github.com/KellyTeresa/gender-friendly-adventures"
   ],
   [
     "DollarDrain (Group Project)",
-    "https://github.com/kaylarr/dollardrains",
-    true
+    "https://dollardrain.herokuapp.com",
+    true,
+    "https://github.com/kaylarr/dollardrains"
   ],
-  ["ChocoWeb 2.0", "http://chocoweb.herokuapp.com/", true],
+  [
+    "ChocoWeb 2.0",
+    "http://chocoweb.herokuapp.com/",
+    true,
+    "https://github.com/KellyTeresa/chocoweb"
+  ],
   ["LinkedIn", "https://www.linkedin.com/in/kraila", false]
 ]
 
 link_list.each do |link|
   Link.create_with(
-    name: link[0], project: link[2]
+    name: link[0], project: link[2], source_url: link[3]
   ).find_or_create_by(url: link[1])
 end
