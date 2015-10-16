865d1d517a444e2eba20b5bd34865b832b0d2aaf
Kelly Raila <kelly.raila@gmail.com>
2015-10-15 23:26:01 -0400

    fix hash syntax

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -3,8 +3,8 @@ class HomesController < ApplicationController
     @projects = Link.where(project: true)
     @other = Link.where(project: false).order('name ASC')
 
-    client = Tumblr::Client.new :consumer_key => ENV["TUMBLR_AUTH"]
-    most_recent_post_data = client.posts 'kellyraila.tumblr.com', :limit => 1
+    client = Tumblr::Client.new consumer_key: ENV["TUMBLR_AUTH"]
+    most_recent_post_data = client.posts 'kellyraila.tumblr.com', limit: 1
     @post = most_recent_post_data["posts"][0]
   end
 end
